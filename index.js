cac6af0a0a802b90b8914339965e3fa0847c3fb9
ddarbouze <ddarbouze@users.noreply.github.com>
2022-10-26 23:31:57 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+let home = 0 
+let guest =  0
+
+function homeAddOne(){
+    home+=1
+    document.getElementById("homescore").textContent =home
+}
+
+function homeAddTwo(){
+    home+=2
+    document.getElementById("homescore").textContent =home
+}
+
+function homeAddThree(){
+    home+=3
+    document.getElementById("homescore").textContent =home
+}
+
+function guestAddOne(){
+    guest+=1
+    document.getElementById("guestscore").textContent =guest
+}
+
+function guestAddTwo(){
+    guest+=2
+    document.getElementById("guestscore").textContent =guest
+}
+
+function guestAddThree(){
+    guest+=3
+    document.getElementById("guestscore").textContent =guest
+}
+
